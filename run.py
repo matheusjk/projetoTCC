1bf51b4fe02993a39d8dffe6b4ffced9c1635fee
matheusjk <matheusrodriguesh1@gmail.com>
2022-03-05 12:30:07 -0300

    executa projeto

--- run.py
+++ run.py
@@ -3,5 +3,5 @@ from app import app
 
 if __name__ == '__main__':
     # app.run()
-    # app.run(host='192.168.0.13', port=59000, ssl_context='adhoc')
-    app.run(host='192.168.0.13', port=59000)
+    app.run(host='192.168.0.13', port=5000, ssl_context='adhoc')
+    # app.run(host='192.168.0.13', port=59000)
